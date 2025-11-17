8feed37f782cb8ef2a3e98be4976fec3aa413a5d
CronusLDX <joaopedrobdf9@gmail.com>
2025-11-17 20:15:44 -0300

    fix: fixing url imports and dependencies on module benchmark

--- src/core/post/repositories/post.repository.ts
+++ src/core/post/repositories/post.repository.ts
@@ -1,7 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/services/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostDocument } from 'database/posts/post.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class PostRepository {
-  constructor(@Inject(PrismaService) private prisma: PrismaService) {}
+  constructor(@InjectModel(Post.name) private postModel: Model<PostDocument>) {}
 }
--- src/core/post/services/post.service.ts
+++ src/core/post/services/post.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Post, PostDocument } from 'database/posts/post.schema';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class PostService {
-  constructor(@InjectModel(Post.name) private postModel: Model<PostDocument>) {}
+  constructor() {}
 }
